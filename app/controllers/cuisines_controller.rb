2dd938bafab0fc1cb09597a86d98a4da5b43202d
Campus Code 08 <campuscode08@campuss-mini-6.home>
2018-02-19 22:15:04 -0300

    Administrador cadastra cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -4,4 +4,17 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.find(id)
     @recipes = @cuisine.recipes
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    name_params = params.require(:cuisine).permit(:name)
+    cuisine = Cuisine.new name_params
+
+    cuisine.save
+
+    redirect_to cuisine_path cuisine
+  end
 end
